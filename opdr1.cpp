950388e1cc7259c4d88022249f92f76b50275583
B Vil <villeriusborro@gmail.com>
2025-02-19 11:18:54 +0100

    fix some formating issues

--- opdr1.cpp
+++ opdr1.cpp
@@ -4,16 +4,14 @@
 #include <algorithm>
 using namespace std;
 
-int main()
-{
+int main() {
     ifstream infile("opdr1.txt");
     string word, currentWord;
     // set word to the first line since that is the word to check against
     getline(infile, word);
-    int charactersInWord[26] = {0};
+    int charactersInWord[26] = { 0 };
     // count characters in the correct word
-    for (char character : word) 
-    {
+    for (char character : word) {
         charactersInWord[character - 'a']++;
     }
     vector<string> testedWords;
@@ -22,37 +20,28 @@ int main()
     int duplicateWordCount = 0;
     int badWordCount = 0;
     // read every other words and check them
-    while (getline(infile, currentWord))
-    {
-        if (currentWord == "STOP")
-        {
+    while (getline(infile, currentWord)) {
+        if (currentWord == "STOP") {
             break;
         }
         wordCount++;
         // if the current word is already checked once count is as a duplicate
-        if (find(testedWords.begin(), testedWords.end(), currentWord) != testedWords.end()) 
-        {
+        if (find(testedWords.begin(), testedWords.end(), currentWord) != testedWords.end()) {
             duplicateWordCount++;
         }
         // if the current word is longer then the current word count word as a bad word because if its longer it cant be made from the same letters
-        else if (currentWord.length() > word.length())
-        {
+        else if (currentWord.length() > word.length()) {
             badWordCount++;
-        }
-        else // otherwise check if the current word can be made from the same letters as the word
+        } else // otherwise check if the current word can be made from the same letters as the word
         {
             int currentTestCharactersInWord[26];
             // copy the array of characters so we can make changes to it with out it interfearing with later words
             copy(charactersInWord, charactersInWord + 26, currentTestCharactersInWord);
             // check if all characters in the current word are in the correct word
-            for (char character : currentWord)
-            {
-                if (currentTestCharactersInWord[character - 'a'] > 0)
-                {
+            for (char character : currentWord) {
+                if (currentTestCharactersInWord[character - 'a'] > 0) {
                     currentTestCharactersInWord[character - 'a']--;
-                }
-                else
-                {
+                } else {
                     badWordCount++;
                     break;
                 }
@@ -63,10 +52,10 @@ int main()
     }
     // if a word is not duplicated and is not a bad word it must be correct and unique
     uniqueGoodWordCount = wordCount - duplicateWordCount - badWordCount;
-    cout << wordCount << endl; //Total words
-    cout << uniqueGoodWordCount << endl; //Unique good words
-    cout << duplicateWordCount << endl; //Same words
-    cout << badWordCount << endl; //Bad words
+    cout << wordCount << endl;           // Total words
+    cout << uniqueGoodWordCount << endl; // Unique good words
+    cout << duplicateWordCount << endl;  // Same words
+    cout << badWordCount << endl;        // Bad words
     infile.close();
     return 0;
 }
