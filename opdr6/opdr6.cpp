730f61aa7aa62a540fb2c7a7fac4045fdc5d635c
B Vil <villeriusborro@gmail.com>
2025-02-24 15:51:20 +0100

    do some more stuff on opdr 6

--- opdr6/opdr6.cpp
+++ opdr6/opdr6.cpp
@@ -12,16 +12,18 @@ white        37         47
 #include <iostream>
 #include <algorithm>
 #include <unistd.h>
+#include <chrono>
+#include <thread>
 using namespace std;
 const string reset = "\033[0m";
-const string items[16] = { "cheese","cucumber","salad","melon","bread","milk","chips","cookies","pork","ham","tomatoes","potatoes","carrots","pizza","banana","somthing" };
+const string items[16] = { "cheese","cucumber","salad","melon","bread","milk","chips","cookies","pork","ham","tomatoes","potatoes","carrots","pizza","banana","paprika" };
 int itemPositions[16] = { 4,5,7,8,18,19,21,22,35,36,38,39,46,47,49,50 };
 bool basket[4][16] = { {false} };
 int basketPositions[4] = { 6,20,37,48 };
 const string playercolors[4] = { "\033[31m","\033[32m","\033[33m","\033[34m" };
 int playerPositions[4] = { 0 };
 int players;
-int turn =0;
+int turn = 0;
 int turnStep = 0;
 
 string setLength(string str, int length) {
@@ -36,19 +38,27 @@ string setLength(int num, int length) {
 }
 bool isInputAvailable() {
     fd_set fds;
-    struct timeval timeout = {0, 0}; // No wait time
+    struct timeval timeout = { 0, 0 }; // No wait time
 
     FD_ZERO(&fds);
     FD_SET(STDIN_FILENO, &fds);
 
     return select(STDIN_FILENO + 1, &fds, nullptr, nullptr, &timeout) > 0;
 }
-void drawboard(int turn) {
+string block(bool draw) {
+    return draw ? "████" : "    ";
+}
+int diceRoll = 0;
+void drawboard(int turn, int turnStep) {
     string board = "";
     board += "\033[s";
     // draw a grid of numbers 
+    if (turnStep == 0) {
+        diceRoll = chrono::duration_cast<std::chrono::nanoseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count() % 6 + 1;
+    }
     int lastPlayerBasketIndex[4] = { 0 };
     for (int i = 0; i < 8; i++) {
+        //draw the board
         for (int j = 0; j < 8; j++) {
             int currentPosition = i * 8 + j + 1;
             string forgroundColor = "";
@@ -60,19 +70,18 @@ void drawboard(int turn) {
                 }
             }
             if (find(itemPositions, itemPositions + 16, currentPosition) != itemPositions + 16) {
-                backgroundColor = "\033[45m";
+                backgroundColor = "\033[105m";
             } else if (find(basketPositions, basketPositions + 4, currentPosition) != basketPositions + 4) {
-                backgroundColor = "\033[46m";
+                backgroundColor = "\033[106m";
             }
             board += backgroundColor + forgroundColor + "|" + ((i * 8 + j + 1) < 10 ? " " : "") + to_string(currentPosition) + "|" + reset + "  ";
         }
-        switch (i) {
-        case 0:
+        // draw the shopping lists
+        if (i == 0) {
             for (int j = 0; j < players; j++) {
                 board += playercolors[j] + "player " + to_string(j + 1) + " (" + to_string(playerPositions[j]) + ")\t" + reset;
             }
-            break;
-        default:
+        } else {
             for (int j = 0; j < players; j++) {
                 string item = "";
                 int position = 0;
@@ -87,16 +96,38 @@ void drawboard(int turn) {
                 board += playercolors[j] + setLength(item, 9);
                 if (position > 0) {
                     board += "(" + setLength(position, 2) + ")";
+                } else{
+                    board += string(4,' ');
                 }
                 board += "\t" + reset;
             }
         }
-        board += "\n";
+        // draw the dice
+        if (turnStep == 0 || turnStep == 1) {
+            board += playercolors[turn];
+            if (i < 2) {
+                board += block(diceRoll > 1) + string(10, ' ') + block(diceRoll > 3);
+            } else if (i > 2 && i < 5) {
+                board += block(diceRoll > 5) + string(3, ' ') + block(diceRoll & 1) + string(3, ' ') + block(diceRoll > 5);
+            } else if (i > 5) {
+                board += block(diceRoll > 3) + string(10, ' ') + block(diceRoll > 1);
+            }
+        }
+        board += reset + "\n";
     }
+    //get nano seconds and calculate the draw time
     cout << board + "\033[u";
+    return;
 }
 int main() {
     srand(time(0));
+    for (int i = 0; i < 16; i++) {
+        int currentItem = rand() % 16;
+        while (basket[i % 4][currentItem]) {
+            currentItem = rand() % 16;
+        }
+        basket[i % 4][currentItem] = true;
+    }
     cout << "how many players 2-4" << endl;
     cin >> players;
     cout << "\033[2A"; // remove those lines again
@@ -105,31 +136,50 @@ int main() {
         cout << "Invalid number of players. Please enter a number between 2 and 4." << endl;
         return 1;
     }
-    for (int i = 0; i < 16; i++) {
-        int currentItem = rand() % 16;
-        while (basket[i % 4][currentItem]) {
-            currentItem = rand() % 16;
-        }
-        basket[i % 4][currentItem] = true;
-    }
-    for (int i = 0; i < players; i++) {
-        cout << "Player " << i + 1 << " has the following items:" << endl;
-        for (int j = 0; j < 16; j++) {
-            if (basket[i][j]) {
-                cout << items[j] << endl;
-            }
-        }
-        cout << endl;
-    }
-    for (int i = 0; i < 4; i++) {
-        playerPositions[i] = rand() % 64;
-        cout << playerPositions[i] << endl;
-    }
     while (true) {
-        drawboard(turn&players);
-        // isInputAvailable()
-        // wait a second
-        sleep(1 / 5);
+        auto start = std::chrono::high_resolution_clock::now();
+        drawboard(turn % players, turnStep);
+        if (isInputAvailable()) {
+            string input;
+            getline(cin, input);
+            turnStep++;
+        }
+        if (turnStep == 1) {
+            diceRoll = rand() % 6 +1;
+            drawboard(turn % players, turnStep);
+            sleep(1);
+            turnStep++;
+        }
+        if (turnStep == 2) {
+            playerPositions[turn % players] += diceRoll;
+            for (int i = 0; i < players; i++){
+                if (turn % players != i){
+                    if (playerPositions[i] == playerPositions[turn % players]){
+                        playerPositions[i] = 0;
+                    }
+                }
+            }
+            for (int i = 0; i <16; i++){
+                if (basket[turn % players][i] && playerPositions[turn % players] == itemPositions[i]){
+                    basket[turn % players][i] = false;
+                }
+            }
+            turnStep++;
+        }
+        if (turnStep > 2) {
+            turnStep = 0;
+            while (isInputAvailable()){
+                string input;
+                getline(cin, input);
+            }
+            turn++;
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        auto remaining_time = 100 - elapsed;
+        if (remaining_time > 0) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(remaining_time));
+        }
     }
 
     return 0;
